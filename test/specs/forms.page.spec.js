8e217a35a2596a0f6e7cd15bfbd05ea4b408e0a6
janekimpak <43620921+janekimpak@users.noreply.github.com>
2022-02-28 23:24:22 -0500

    first test

---
+++ test/specs/forms.page.spec.js
@@ -0,0 +1,7 @@
+const LoginPage = require('../pageobjects/login.page');
+
+describe('Forms Page', () => {
+    it('User should be able to redirect to forms page ', async() => {
+        await
+    });
+});
